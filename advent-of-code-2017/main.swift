28c1444d8ca4759a84e3c652869cf9d56bd07f4e
Daniel Ma <drailskid@yahoo.com>
2017-12-01 10:30:03 -0800

    Initial Commit

---
+++ advent-of-code-2017/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  advent-of-code-2017
+//
+//  Created by Daniel Ma on 12/1/17.
+//  Copyright © 2017 DMa Unsure. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
