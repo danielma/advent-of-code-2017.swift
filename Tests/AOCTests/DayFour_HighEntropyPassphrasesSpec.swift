f3baac1702d7b13b873af238ed49ab3f69a9bf08
Daniel Ma <drailskid@yahoo.com>
2017-12-03 22:28:30 -0800

    day 4, part 2

--- Tests/AOCTests/DayFour_HighEntropyPassphrasesSpec.swift
+++ Tests/AOCTests/DayFour_HighEntropyPassphrasesSpec.swift
@@ -4,17 +4,39 @@ import Nimble
 
 class DayFour_HighEntropyPassphrasesSpec: QuickSpec {
   override func spec() {
-    fdescribe("part 1") {
+    describe("part 1") {
       it("aa bb cc dd ee") {
-        expect(HighEntropyPassphrases.isValid("aa bb cc dd ee")) == true
+        expect(HighEntropyPassphrases.isValidPartOne("aa bb cc dd ee")) == true
       }
 
       it("aa bb cc dd aa") {
-        expect(HighEntropyPassphrases.isValid("aa bb cc dd aa")) == false
+        expect(HighEntropyPassphrases.isValidPartOne("aa bb cc dd aa")) == false
       }
 
       it("aa bb cc dd aaa") {
-        expect(HighEntropyPassphrases.isValid("aa bb cc dd aaa")) == true
+        expect(HighEntropyPassphrases.isValidPartOne("aa bb cc dd aaa")) == true
+      }
+    }
+
+    describe("part 2") {
+      it("abcde fghij") {
+        expect(HighEntropyPassphrases.isValidPartTwo("abcde fghij")) == true
+      }
+
+      it("abcde xyz ecdab") {
+        expect(HighEntropyPassphrases.isValidPartTwo("abcde xyz ecdab")) == false
+      }
+
+      it("a ab abc abd abf abj") {
+        expect(HighEntropyPassphrases.isValidPartTwo("a ab abc abd abf abj")) == true
+      }
+
+      it("iiii oiii ooii oooi oooo") {
+        expect(HighEntropyPassphrases.isValidPartTwo("iiii oiii ooii oooi oooo")) == true
+      }
+
+      it("oiii ioii iioi iiio") {
+        expect(HighEntropyPassphrases.isValidPartTwo("oiii ioii iioi iiio")) == false
       }
     }
   }
